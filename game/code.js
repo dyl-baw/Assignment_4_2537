2a386f279bb2e4cd54d3f08e8143452ac6db42b1
David Lee <dvd.y.lee@gmail.com>
2022-05-26 01:11:16 -0700

    Feat: working on shuffling cards

--- game/code.js
+++ game/code.js
@@ -1,4 +1,3 @@
-
 // async function loadCards() {
 //     await $.ajax({
 //         type:"GET",
@@ -9,9 +8,10 @@
 
 
 hasFlippedCard = false;
-
+lockBoard = false;
 firstCard = undefined;
 secondCard = undefined;
+pairsFound = 0;
 
 // const cards = document.querySelectorAll('.card');
 
@@ -26,38 +26,63 @@ function setup() {
 
     // loadCards();
     $(".card").on("click", function () {
+        if(lockBoard) return;
+        if(this == firstCard) return;
+        if(!this.classList.contains("flip")){
         $(this).toggleClass("flip")
 
-        if(!hasFlippedCard){
-            // this is the first card
+        if (!hasFlippedCard) {
             firstCard = $(this).find('.front_face')[0]
-            // console.log(firstCard);
             hasFlippedCard = true;
-        }else{
+        } else {
             // 2nd card
-            secondCard =  $(this).find('.front_face')[0]
+            secondCard = $(this).find('.front_face')[0]
             console.log(firstCard, secondCard);
             hasFlippedCard = false;
-
-            // check if you have match
-            if (
-                $(`#${firstCard.id}`).attr("src")
-                ==
-                $(`#${secondCard.id}`).attr("src")
-            ) {
-                console.log("A Match!");
-                $(`#${firstCard.id}`).parent().off("click")
-                $(`#${secondCard.id}`).parent().off("click")
-            } else {
-                console.log("not a Match!");
-                setTimeout( () => {
-                    $(`#${firstCard.id}`).parent().removeClass("flip")
-                    $(`#${secondCard.id}`).parent().removeClass("flip")
-                }, 1000);
-
-            }
+            checkForMatch();
         }
-    })
+}})
+
+    function checkForMatch() {
+        // check if you have match
+        if (
+            $(`#${firstCard.id}`).attr("src") ==
+            $(`#${secondCard.id}`).attr("src")
+        ) {
+            console.log("A Match!");
+            pairsFound++;
+            disableCards();
+
+        } else {
+            console.log("not a Match!");
+            unflipCards();
+        }
+    }
+
+    function disableCards() {
+        $(`#${firstCard.id}`).parent().off("click")
+        $(`#${secondCard.id}`).parent().off("click")
+        resetBoard();
+    }
+
+    function unflipCards() {
+        lockBoard = true;
+        setTimeout(() => {
+            $(`#${firstCard.id}`).parent().removeClass("flip")
+            $(`#${secondCard.id}`).parent().removeClass("flip")
+            resetBoard();
+        }, 1000);
+    }
+
+    function resetBoard() {
+        [hasFlippedCard, lockBoard] = [false, false];
+        [firstCard, secondCard] = [null, null];
+    }
+
+    // (function shuffle(){
+    //     let randomPosition = Math.floor(Math.random() * 12);
+    //     $('.card').css("order", randomPosition);
+    // })();
 }
 
 $(document).ready(setup)
